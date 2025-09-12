fc108bfee3d46d4a9414f35ea0083500cf26b5a3
valmri <valmarmie87@outlook.com>
2025-09-12 10:33:17 +0200

    init

---
+++ src/app/contact/layout.tsx
@@ -0,0 +1,10 @@
+const ContactLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <p>Page de contact</p>
+      {children}
+    </>
+  );
+};
+
+export default ContactLayout;
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,5 @@
+const contactPage = () => {
+  return <>coucou</>;
+};
+
+export default contactPage;
